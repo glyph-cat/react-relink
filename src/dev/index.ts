cf65ae69d6649f2d6c4b88ef5c088011e3faeaec
chin98edwin <edwin_chin98@hotmail.com>
2021-09-04 19:20:35 +0800

    Code cleanup

--- src/dev/index.ts
+++ src/dev/index.ts
@@ -1,43 +1,16 @@
 /* eslint-disable no-console */
 import { IS_DEBUG_ENV } from '../constants'
 
-type DevConsoleMethods = keyof typeof console
-
-if (IS_DEBUG_ENV) {
-  var onlyOnceCache = {
-    // TODO: Make this dynamic
-    log: {},
-    info: {},
-    warn: {},
-    error: {},
-  }
-}
-
-function devPrint(type: DevConsoleMethods, message: string): void {
-  if (IS_DEBUG_ENV) {
-    console[type](`[Development] ${message}`)
-  }
-}
-
-// TODO: Export `devLog` etc to reduce declaration of 'log'
-
-export function devPrintOnce(
-  type: DevConsoleMethods,
-  key: string,
-  message: string
+function devPrint(
+  type: keyof typeof console,
+  ...args: unknown[]
 ): void {
   if (IS_DEBUG_ENV) {
-    if (!onlyOnceCache[type][key]) {
-      onlyOnceCache[type][key] = true
-      console[type](`[Development] ${message}`)
-    }
+    // @ts-ignore
+    console[type]('[Development]', ...args)
   }
 }
 
-export function deprecationWarn(key: string, message: string): void {
-  devPrintOnce('warn', key, `Deprecation warning: ${message}`)
-}
-
-export function devError(message: string): void {
-  devPrint('error', message)
+export function devError(...args: unknown[]): void {
+  devPrint('error', ...args)
 }
