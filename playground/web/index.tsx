260889ab2a2b0de151450af05f20ba52e5b182c7
glyph-cat <b75ggnacj@relay.firefox.com>
2021-12-27 15:10:19 +0800

    Added playground containers for cyclic-call checking

--- playground/web/index.tsx
+++ playground/web/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Playground } from './containers/cyclic-calls'
 // import { Playground } from './containers/generic'
-import { Playground } from './containers/scope'
+// import { Playground } from './containers/scope'
 // import { Playground } from './containers/use-relink-state'
 
 ReactDOM.render(
