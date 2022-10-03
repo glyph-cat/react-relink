38a655e892dd5687ed1c95f11e179b29405cbbd4
glyph-cat <b75ggnacj@relay.firefox.com>
2022-10-03 22:23:54 +0800

    Enabled `scripts/detete-e2e-screenshots`

--- scripts/detete-e2e-screenshots/index.ts
+++ scripts/detete-e2e-screenshots/index.ts
@@ -5,5 +5,5 @@ const BASE_PATH = './tests/e2e/'
 const dirStack = fs.readdirSync(BASE_PATH)
 for (const item of dirStack) {
   if (/\./g.test(item)) { continue }
-  // fs.rmSync(`${BASE_PATH}/${item}/screenshots`, { recursive: true })
+  fs.rmSync(`${BASE_PATH}/${item}/screenshots`, { recursive: true })
 }
