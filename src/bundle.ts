fc259003f5c5c0dcb3a1fba3f4ff610a541da37c
glyph-cat <b75ggnacj@relay.firefox.com>
2024-06-03 11:48:52 +0800

    Added package deprecation message

--- src/bundle.ts
+++ src/bundle.ts
@@ -1,3 +1,5 @@
+import { IS_DEV_ENV } from './constants'
+
 export * from './abstractions'
 export * from './api/compare-fn-presets'
 export * from './api/config'
@@ -13,3 +15,7 @@ export * from './api/use-set-relink-state'
 export * from './api/wait-for'
 export * from './constants/public'
 
+if (IS_DEV_ENV) {
+  // eslint-disable-next-line no-console
+  console.warn('This package has been deprecated in favor of a cleaner and more efficient alternative: \'cotton-box\'. The migration guide is available at: https://glyph-cat.github.io/cotton-box/docs/learn/react/migration-from-react-relink')
+}
